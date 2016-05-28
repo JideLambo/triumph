f97c467cc7ff7356dcb52699e9f8517aa88ab0a4
Jide Lambo <jide.lambo@hcntech.com>
2016-05-28 11:03:25 +0100

    add files here

---
+++ include/header.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Triumph Power & Gas Systems Limited has been appointed as the exclusive agent in Nigeria of Dunlop Oil & Marine Limited which is recognized as a leading manufacturer of floating and submarine hoses to all major oil companies worldwide.">
+    <meta name="author" content="developed by Jide Lambo for Deefrent Limited, Lagos">
+    <meta name="Keywords" content="Triumph Power & Gas Systems,Dunlop Oil Marine, hoses, oil, marine, offshore marine hoses, mitwater system, dock shore hoses, dredge hoses sleeves, single carcass, double carcass, loading hose applications, pressure telemetry system, ancillary equipment, Fluid Technology, Dunlop Oil and Marine Ltd.">
+    <title>Triumph - <?php if(isset($page_title)) {echo $page_title;} ?></title>
+    <link rel="shortcut icon" href="">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/animate.css">
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,500,700' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+
+
+    <!--[if IE]>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand menu-logo" href="#">
+                </a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="navbar">
+                <ul class="nav navbar-nav">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="about.html">About Us</a></li>
+                    <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Solutions &amp; services <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">OUR SERVICES</a></li>
+                            <li><a href="#">OUR CUSTOMERS</a></li>
+                            <li><a href="#">POLICY STATEMENT</a></li>
+
+                        </ul>
+                    </li>
+                    <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Resource <b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">News</a></li>
+                            <li><a href="#">Articles</a></li>
+                            <li><a href="#">Case Studies</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="#">Contact us</a></li>
+                </ul>
+            </div><!--.nav-collapse -->
+        </div>
+    </nav>
